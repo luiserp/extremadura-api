4bd02d252837583ffc8c08d0c10d89fa98de7aca
luiserp <luis@taxo.co>
2024-10-23 07:00:28 -0400

    get embeddings

--- app/controllers/ollama/ollama_controller.py
+++ app/controllers/ollama/ollama_controller.py
@@ -1,15 +1,31 @@
 from ollama import Client
 import os
-
+from sklearn.decomposition import PCA
+import numpy as np
 class OllamaController:
     
     def __init__(self):
         host = os.getenv("OLLAMA_HOST")
         self.client = Client(host=host)
+        self.model_name = 'llama3.2'
 
     def get_description(self, text):
-        model_name = 'TextDescriptor'
         return {
-            "description": self.client.generate(model=model_name, prompt=text)["response"],
-            "model": model_name
+            "description": self.client.generate(model=self.model_name, prompt=text)["response"],
+            "model": self.model_name
+        }
+
+
+    def get_embbeding(self, text):
+        return {
+            "embeddings": self.client.embed(model=self.model_name, input=text),
+        }
+        
+    def get_normalized_embbeding(self, text, n_components=2):
+        embeddings = self.client.embed(model=self.model_name, input=text)["embeddings"]
+        embeddings_array = np.array(embeddings)
+        embeddings_normalized = PCA(n_components).fit_transform(embeddings)
+        return {
+            "embeddings": embeddings,
+            "embeddings_normalized": embeddings_normalized
         }
--- app/routes/ollama.py
+++ app/routes/ollama.py
@@ -13,3 +13,12 @@ def router(app):
     @app.post("/description")
     def description(input: Input):
         return OllamaController().get_description(input.text)
+    
+    @app.post("/embbeding")
+    def embbeding(input: Input):
+        return OllamaController().get_embbeding(input.text)
+    
+
+    @app.post("/normalized_embbeding")
+    def normalized_embbeding(input: Input):
+        return OllamaController().get_normalized_embbeding(input.text, input.n_components if hasattr(input, 'n_components') else 1)
